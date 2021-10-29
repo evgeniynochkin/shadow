9ece3b993092c1a2ad6b62d5c7c8182724bb9e16
Evgeniy Nochkin <nochjo@yandex.ru>
2021-10-29 06:54:38 +0300

    Commit

--- src/main/java/com/oaksheet/shadow/model/User.java
+++ src/main/java/com/oaksheet/shadow/model/User.java
@@ -1,11 +1,42 @@
 package com.oaksheet.shadow.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String email;
+
+    private Long password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPassword() {
+        return password;
+    }
+
+    public void setPassword(Long password) {
+        this.password = password;
+    }
 }
